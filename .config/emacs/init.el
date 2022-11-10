7b3a94a25f96bc34809762c6bd10e61b5bc34097
Jack3G <40290244+Jack3G@users.noreply.github.com>
2022-11-10 14:00:03 +1100

    Add emacs to dotfiles

---
+++ .config/emacs/init.el
@@ -0,0 +1,27 @@
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+(package-refresh-contents)
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
+
+
+(use-package auto-package-update
+  :config
+  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-hide-results t)
+  (auto-package-update-maybe))
+
+
+(use-package evil
+    :config
+    (require 'evil)
+    (evil-mode 1))

e22dacebf54c86fa04b47f66f36ca94d1550146b
Jack3G <40290244+Jack3G@users.noreply.github.com>
2022-11-12 07:29:31 +1100

    wip...
    theme, default-frame-alist, backups directory, formatting

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -10,6 +10,7 @@
 (eval-when-compile
   (require 'use-package))
 
+
 (require 'use-package-ensure)
 (setq use-package-always-ensure t)
 
@@ -20,8 +21,37 @@
   (setq auto-package-update-hide-results t)
   (auto-package-update-maybe))
 
-
 (use-package evil
-    :config
-    (require 'evil)
-    (evil-mode 1))
+  :config
+  (require 'evil)
+  (evil-mode 1))
+
+
+(load-theme 'tango-dark)
+
+(setq default-frame-alist
+      '((tool-bar-lines . 0)
+	(vertical-scroll-bars . nil)))
+
+;; yoink https://www.emacswiki.org/emacs/BackupDirectory
+(setq
+ backup-by-copying t
+ backup-directory-alist '(("." . "~/backup/emacs-backups/"))
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+ version-control t)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(evil auto-package-update use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

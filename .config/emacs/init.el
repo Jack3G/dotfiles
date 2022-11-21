b58a1efb637609c9f4ab564acaa2b6272d12b8ee
Jack3G <40290244+Jack3G@users.noreply.github.com>
2022-11-21 15:49:00 +1100

    (emacs) stop auto-updating packages

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -22,8 +22,8 @@
 (use-package auto-package-update
   :config
   (setq auto-package-update-delete-old-versions t)
-  (setq auto-package-update-hide-results t)
-  (auto-package-update-maybe))
+  (setq auto-package-update-hide-results t))
+
 
 (use-package evil
   :init
